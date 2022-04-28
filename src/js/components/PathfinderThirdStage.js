2b5db28c0ce3d82710d716a1468c869883ab2aa6
Milosz Bargiel <miloszbargiel@gmail.com>
2022-04-28 14:04:26 +0200

    Add starting files for 3rd stage of an app

---
+++ src/js/components/PathfinderThirdStage.js
@@ -0,0 +1,11 @@
+// import { select, settings, classNames } from '../settings.js';
+
+class PathfinderThirdStage {
+  constructor(data) {
+    this.route = data.route;
+    this.selectedPoints = data.selectedPoints;
+    this.shortestPath = [];
+  }
+}
+
+export default PathfinderThirdStage;
