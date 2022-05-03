7411a247a5995f44e14d96c4daa8125d0c939069
Milosz Bargiel <miloszbargiel@gmail.com>
2022-05-03 18:04:42 +0200

    Add initReset and change naming convention

--- src/js/script.js
+++ src/js/script.js
@@ -4,23 +4,22 @@ import Pathfinder from './components/Pathfinder.js';
 const app = {
 
   init: function() {
-    const thisApp = this;
-    thisApp.initPages();
-    thisApp.initPathfinder();
+    this.initPages();
+    this.initPathfinder();
+    this.initReset(); 
   },
 
   initPages: function() {
-    const thisApp = this;
-    thisApp.navWrapper = document.querySelector(select.containerOf.nav);
-    thisApp.navLinks = thisApp.navWrapper.querySelectorAll(select.nav.links);
-    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    this.navWrapper = document.querySelector(select.containerOf.nav);
+    this.navLinks = this.navWrapper.querySelectorAll(select.nav.links);
+    this.pages = document.querySelector(select.containerOf.pages).children;
     const IDFromHash = (window.location.hash).slice(2);
-    for (let link of thisApp.navLinks) {
+    for (let link of this.navLinks) {
       const pageID = link.getAttribute('href').replace('#', '');
       if(IDFromHash === pageID) {
         app.activatePage(IDFromHash);
       } else {
-        app.activatePage(thisApp.navLinks[0].getAttribute('href').replace('#', ''));
+        app.activatePage(this.navLinks[0].getAttribute('href').replace('#', ''));
       }
       link.addEventListener('click', function(e) {
         e.preventDefault();
@@ -30,20 +29,25 @@ const app = {
   },
 
   activatePage: function(pageID) {
-    const thisApp = this;
-    for (let page of thisApp.pages) {
+    for (let page of this.pages) {
       page.classList.toggle(classNames.page.active, page.id === pageID);
     }
-    for (let link of thisApp.navLinks) {
+    for (let link of this.navLinks) {
       link.classList.toggle(classNames.navLink.active, link.getAttribute('href') === '#' + pageID);
     }
     window.location.hash = `/${pageID}`;
   },
 
   initPathfinder: function() {
-    const thisApp = this;
-    const pathfinderWrapper = document.querySelector(select.containerOf.pathfinder);
-    thisApp.pathfinder = new Pathfinder(pathfinderWrapper);
+    this.wrapper = document.querySelector(select.containerOf.pathfinder);
+    this.pathfinder = new Pathfinder(this.wrapper);
+  },
+
+  initReset: function() {
+    this.wrapper.addEventListener('reset', () => {
+      this.wrapper.innerHTML = '';
+      this.pathfinder = new Pathfinder(this.wrapper);
+    });
   }
 
 };
