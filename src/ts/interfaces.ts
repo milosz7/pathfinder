b2adcddc0c3c76129a9993db15c2c55d3a5e6688
Milosz Bargiel <miloszbargiel@gmail.com>
2022-05-24 14:32:29 +0200

    Add interfaces file

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -15,3 +15,39 @@ export interface stageBase {
   cells: Array<cell>;
   route: coordinateArr;
 }
+
+export interface appProps {
+  [key: string]: Function
+}
+
+export interface initPages {
+  navWrapper: HTMLElement;
+  navLinks: NodeListOf<HTMLAnchorElement>;
+  pages: HTMLCollection;
+}
+
+export interface activatePage {
+  pages: HTMLCollection;
+  navLinks: NodeListOf<HTMLAnchorElement>;
+}
+
+export interface initPathfinder {
+  wrapper: HTMLDivElement;
+  pathfinder: object;
+}
+
+export interface initReset {
+  wrapper: HTMLDivElement;
+  pathfinder: object;
+  sideMenuButton: HTMLDivElement;
+  sideMenuControls: HTMLDivElement;
+  sideMenuTitle: HTMLElement;
+}
+
+export interface initButtons {
+  rulesButton: HTMLButtonElement;
+  rulesHideButtons: NodeListOf<HTMLButtonElement>;
+  sideMenuButton: HTMLDivElement;
+  sideMenuControls: HTMLDivElement;
+  sideMenuTitle: HTMLElement;
+}
